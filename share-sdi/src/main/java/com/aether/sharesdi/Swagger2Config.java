51d9b2573cbbd24b3e5e5a840b547c332457af81
liuqinfu <lqf1373900785@foxmail.com>
2020-08-12 17:25:44 +0800

    针对不同服务，接口文档显示不同的接口信息

--- share-sdi/src/main/java/com/aether/sharesdi/Swagger2Config.java
+++ share-sdi/src/main/java/com/aether/sharesdi/Swagger2Config.java
@@ -1,7 +1,16 @@
 package com.aether.sharesdi;
 
 import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
@@ -12,4 +21,26 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 @EnableKnife4j
 public class Swagger2Config {
+
+    @Value("${spring.application.name}")
+    private String applicationName;
+
+    @Bean
+    public Docket createRestfulApi() {//api文档实例
+        return new Docket(DocumentationType.SWAGGER_2)//文档类型：DocumentationType.SWAGGER_2
+                .apiInfo(apiInfo())//api信息
+                .select()//构建api选择器
+                .apis(RequestHandlerSelectors.basePackage("com.aether.sharesdi.controller"))//api选择器选择api的包
+                .paths(PathSelectors.any())//api选择器选择包路径下任何api显示在文档中
+                .build();//创建文档
+    }
+
+    private ApiInfo apiInfo() {//接口的相关信息
+        return new ApiInfoBuilder()
+                .title(applicationName+"后台服务接口文档")
+                .description("接口文档")
+                .contact(new Contact("liuqinfu","liuqf@s-ec.com", "liuqf@s-ec.com"))
+                .version("1.0")
+                .build();
+    }
 }
